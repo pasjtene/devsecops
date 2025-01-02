6e543f6905b910e717f45ed74721d93126961639
Pascal JT <pasjtene@yahoo.co.uk>
2025-01-01 23:13:57 -0500

    login

--- globepy/globepy/views.py
+++ globepy/globepy/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
 def say_hello(request):
     return HttpResponse('Hello world...!!! <br/> Another hello world <br> link <a href="/lms/hello2">go to hello2 </a>')
 
+def is_in_group(user, group_name):
+    return user.groups.filter(name=group_name).exist()
+
+@login_required
+@user_passes_test(lambda user: is_in_group(user, 'grcusers'))
 def say_hello2(request):
     response_data = {}
     response_data["erro"] = "No error found"
