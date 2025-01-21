a9aa64ae4f2b13eaae472acd2750898a94cec543
Pascal JT <pasjtene@yahoo.co.uk>
2025-01-20 20:36:40 -0500

    login

--- globepy/accounts/urls.py
+++ globepy/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 from .views import SignUpView
+
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", views.login, name="nicelogin")
 ]
--- globepy/accounts/views.py
+++ globepy/accounts/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.shortcuts import render
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+    
+def login(request):
+    return render(request, "registration/login.html")
