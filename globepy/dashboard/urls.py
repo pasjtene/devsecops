85251fc2833728bbbce0df6464f5a9461b148214
Pascal JT <pasjtene@yahoo.co.uk>
2025-01-23 22:02:20 -0500

    dashboard

--- globepy/dashboard/urls.py
+++ globepy/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.dashboard_home, name='dashboard_home'),
+    path('home/', views.dashboardhome),
     path('hello2/', views.say_hello2),
     path('hello3/', views.say_hello3),
     path('hello4/', views.say_hello4)
