d140b113a093d8c9b06bef586e56d7b80e8e2556
Pascal JT <pasjtene@yahoo.co.uk>
2025-01-31 22:32:09 +0100

    create products app

--- globepy/products/models.py
+++ globepy/products/models.py
@@ -3,8 +3,8 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres import fields as PostgresFields
 
 class ProductCategory(models.Model):
-    name = models.CharFields(max_length=256)
-    icon_url = models.URLFields(blank=True)
+    name = models.CharField(max_length=256)
+    icon_url = models.URLField(blank=True)
     description = models.TextField()
     parent_category = models.ForeignKey(
         'self',
