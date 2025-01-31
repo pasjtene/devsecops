9e6c2474c90d36bc7ec64a48797e29aa593ebc13
Pascal JT <pasjtene@yahoo.co.uk>
2025-01-31 22:28:04 +0100

    create products app

--- globepy/products/models.py
+++ globepy/products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.postgres import fields as PostgresFields
 
+class ProductCategory(models.Model):
+    name = models.CharFields(max_length=256)
+    icon_url = models.URLFields(blank=True)
+    description = models.TextField()
+    parent_category = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        related_name="children_categories",
+        on_delete = models.CASCADE
+    )
 # Create your models here.
