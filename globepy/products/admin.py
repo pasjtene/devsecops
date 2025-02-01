aa4b438a06a3ddd8e422ba3a327640cd22e758fb
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-01 01:43:51 +0100

    create products app

--- globepy/products/admin.py
+++ globepy/products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(ProductCategory)
+#admin.site.register(ProductCategory)
 admin.site.register(Vendor)
-admin.site.register(Product)
+#admin.site.register(Product)
 
 # Register your models here.
