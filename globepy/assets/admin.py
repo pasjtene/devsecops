8f4f354cc1ab2f7a746ef5b1817411d0066d7dda
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-02 11:05:30 +0100

    Create assets app

--- globepy/assets/admin.py
+++ globepy/assets/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 #admin.site.register(ProductCategory)
-#admin.site.register(Vendor)
+admin.site.register(Vendor)
 #admin.site.register(Product)
 
 # Register your models here.
