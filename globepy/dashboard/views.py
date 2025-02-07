e6869cf7a0f438938d34fd111609fe007c874aa4
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-07 22:22:40 +0100

    added risk status and impact level

--- globepy/dashboard/views.py
+++ globepy/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required, user_passes_test
+from assets.models import Asset
 
 # Create your views here.
 def formeditors2(request):
@@ -23,7 +24,18 @@ def dashboardhome(request):
     response_data = {}
     response_data["erro"] = "LMS -- No error found"
     response_data["message"] = "Hello world"
-    return render(request, "dashboard/index.html", {"vars": response_data, "name":"Pascal JT"})
+    assets = Asset.objects.all()
+    total_price = sum(asset.price * asset.quantity for asset in assets) #cal culate the total price of all assets
+    total_assets = sum(asset.quantity for asset in assets) # calculate the total number of assets
+    total_unique_assets = assets.count()
+    
+    return render(request, 'assets/assets_dashboard.html', {
+        'assets':assets,
+        'total_price': round(total_price),
+        'total_assets':total_assets,
+        'total_unique_assets':total_unique_assets
+        })
+    #return render(request, "assets/assets_dashboard.html", {"vars": response_data, "name":"Pascal JT"})
 
 def say_hello3(request):
     return JsonResponse({"response":"Hello world json"})
