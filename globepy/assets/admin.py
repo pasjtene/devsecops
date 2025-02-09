7de90f3dd1d7f8525248ef4217f3cbb7efb7d505
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-09 15:12:15 +0100

    Add compliance status for each requirement

--- globepy/assets/admin.py
+++ globepy/assets/admin.py
@@ -5,20 +5,3 @@ from .models import *
 admin.site.register(Vendor)
 admin.site.register(Risk)
 admin.site.register(SecurityRequirement)
-
-class ComplianceStatus(admin.ModelAdmin):
-    list_display = ('asset', 'framework', 'requirement', 'details','owner')
-    list_filter = ('asset','framework',)
-    search_fields = ('framework', 'requirement')
-    
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:  # If the object is being created
-            obj.created_by = request.user
-        super().save_model(request, obj, form, change)
-    
-   
-
-#admin.site.register(Asset, AssetAdmin)
-admin.register(ComplianceStatus)
-
-# Register your models here.
