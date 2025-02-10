bb7075437e5de29cae353e9bf1f381907a037d5c
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-10 15:08:04 +0100

    Add compliance status form

--- globepy/assets/forms.py
+++ globepy/assets/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class AssetForm(forms.ModelForm):
     class Meta:
         model = Asset
-        fields = ['name', 'price', 'quantity', 'description', 'category', 'risk_status', 'risk_level', 'reviewed_by']
+        fields = ['name', 'price', 'quantity', 'description', 'category', 'risk_status', 'impact_level']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3}),
             'reviewed_by': forms.Select(attrs={'class': 'form-control'}),
