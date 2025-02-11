68846f33908514d2a7dc28d1883e526d043f13a4
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-11 21:24:32 +0100

    Add compliance status form

---
+++ globepy/static/niceadmin/assets/js/frameworks-requirements.js
@@ -0,0 +1,30 @@
+/**
+* Created: 2025/02/11
+* 
+* Updated: Apr 20 2024 with Bootstrap v5.3.3
+* Author: Pascal Tene
+* License: To be confirmed (Private)
+*/
+
+(function() {
+    "use strict";
+  
+    /**
+     * Easy selector helper function
+     */
+   
+    function viewRequirementDetails() {
+        $('.requirement-description').click(function(e){
+            e.preventDefaul();
+            console.log("Requirement clicked...!")
+            $(this).closest('.requirement-details').find(".activity-edit").show();
+            $(this).closest('.requirement-details').show();
+            $('#pageusers').show();
+            $('.usersmodal').draggable();
+        });
+    
+       
+    }
+   
+
+  })();
