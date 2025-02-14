2515a68978c74f357ff2be2d64ff3dbacdc80dae
Pascal JT <pasjtene@yahoo.co.uk>
2025-02-14 15:15:39 +0100

    add comment

--- globepy/assets/urls.py
+++ globepy/assets/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('create-page/', views.create_page, name='create-new-page'),
     path('updated-compliance-requirement/<int:id>', views.update_compliance_requirement, name='update-compliance-requirement' ),
     path('create-compliance-requirement/<int:frameworkid>/<int:assetid>/<int:requirementid>', views.create_compliance_requirement, name='create-compliance-requirement' ),
-    path('comments/add/', views.add_comment, name='add_comment'),
-    path('comments/add/<int:parent_id>/', views.add_comment, name='add_reply'),
+    path('comments/add/<int:assetid>/', views.add_comment, name='add_comment'),
+    path('comments/add/<int:assetid>/<int:parent_id>/', views.add_comment, name='add_reply'),
     
 ]
 
